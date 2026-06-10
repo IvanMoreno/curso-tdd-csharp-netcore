fb602b0f8fe29878c8f3af3f65be75f7f26b574f
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2026-06-10 10:13:21 +0200

    docs: example list

--- password-validator/PasswordValidator.Tests/PasswordValidatorTest.cs
+++ password-validator/PasswordValidator.Tests/PasswordValidatorTest.cs
@@ -1,6 +1,26 @@
 using System;
 using NUnit.Framework;
 
+// Observable behavior: Contraseña es validada
+// Input: Contraseña
+// Output: Valido o no (bool)
+
+// Smaller behaviors:
+// Obligatoriamente:
+    // Have more than 8 characters (8, inf)
+    // Una mayúscula como mínimo
+    // Una minúscula como mínimo
+    // Un número como mínimo
+    // Una barra baja como mínimo
+
+// Ejemplos
+// (1Aa_5678) = No válido
+// (1Aa_56789) = Válida
+// (1Aaa56789) = No válido
+// (1A__56789) = No válido
+// (1aa_56789) = No válido
+// (aAa_aaaaa) = No válido
+
 namespace PasswordValidator.Tests
 {
     public class PasswordValidatorTest
