edbf483bb9f9f7895bda5fe78e725538e3604a6a
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2026-06-10 10:41:23 +0200

    refactor: dead code

--- password-validator/PasswordValidator/PasswordValidator.cs
+++ password-validator/PasswordValidator/PasswordValidator.cs
@@ -24,36 +24,6 @@ namespace PasswordValidator
             }
 
             return true;
-            return ContainsNumber(password)
-                   && IsLongEnough(password)
-                   && ContainsUnderscore(password)
-                   && ContainsLowercase(password)
-                   && ContainsUppercase(password);
-        }
-
-        private static bool IsLongEnough(string password)
-        {
-            return password.Length > 8;
-        }
-
-        private static bool ContainsNumber(string password)
-        {
-            return Regex.IsMatch(password, "[0-9]");
-        }
-
-        private static bool ContainsLowercase(string password)
-        {
-            return Regex.IsMatch(password, "[a-z]");
-        }
-
-        private static bool ContainsUppercase(string password)
-        {
-            return Regex.IsMatch(password, "[A-Z]");
-        }
-
-        private static bool ContainsUnderscore(string password)
-        {
-            return password.Contains("_");
         }
 
         public static PasswordValidator Default()
