cbf1f688acb16f5c84028a9395455f57a38bbe68
Ivan Moreno Garcia-Fresneda <ivan.m@klang-games.com>
2026-06-10 10:41:02 +0200

    refactor: extract conditional to polymorphism

--- password-validator/PasswordValidator/PasswordValidator.cs
+++ password-validator/PasswordValidator/PasswordValidator.cs
@@ -1,15 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 namespace PasswordValidator
 {
     public class PasswordValidator
     {
+        private readonly List<Policy> policies;
+
+        private PasswordValidator(List<Policy> policies)
+        {
+            this.policies = policies;
+        }
+
         public bool IsValid(string password)
         {
-            return ContainsNumber(password) 
-                   && IsLongEnough(password) 
-                   && ContainsUnderscore(password) 
+            foreach (var policy in policies)
+            {
+                if (!policy.Satisfies(password))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+            return ContainsNumber(password)
+                   && IsLongEnough(password)
+                   && ContainsUnderscore(password)
                    && ContainsLowercase(password)
                    && ContainsUppercase(password);
         }
@@ -23,12 +40,12 @@ namespace PasswordValidator
         {
             return Regex.IsMatch(password, "[0-9]");
         }
-        
+
         private static bool ContainsLowercase(string password)
         {
             return Regex.IsMatch(password, "[a-z]");
         }
-        
+
         private static bool ContainsUppercase(string password)
         {
             return Regex.IsMatch(password, "[A-Z]");
@@ -37,6 +54,62 @@ namespace PasswordValidator
         private static bool ContainsUnderscore(string password)
         {
             return password.Contains("_");
+        }
+
+        public static PasswordValidator Default()
+        {
+            return new PasswordValidator([
+                new ContainsUnderscore(), 
+                new ContainsUppercase(), 
+                new ContainsLowercase(), 
+                new IsLongEnough(),
+                new ContainsNumber()
+            ]);
+        }
+    }
+
+    public abstract class Policy
+    {
+        public abstract bool Satisfies(string password);
+    }
+
+    public class ContainsUnderscore : Policy
+    {
+        public override bool Satisfies(string password)
+        {
+            return password.Contains("_");
+        }
+    }
+
+    public class ContainsUppercase : Policy
+    {
+        public override bool Satisfies(string password)
+        {
+            return Regex.IsMatch(password, "[A-Z]");
+        }
+    }
+
+    public class ContainsLowercase : Policy
+    {
+        public override bool Satisfies(string password)
+        {
+            return Regex.IsMatch(password, "[a-z]");
+        }
+    }
+
+    public class IsLongEnough : Policy
+    {
+        public override bool Satisfies(string password)
+        {
+            return password.Length > 8;
+        }
+    }
+
+    public class ContainsNumber : Policy
+    {
+        public override bool Satisfies(string password)
+        {
+            return Regex.IsMatch(password, "[0-9]");
         }
     }
 }
