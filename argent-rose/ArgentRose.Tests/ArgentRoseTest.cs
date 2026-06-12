040e4106758235bdccd53ff42b3c6f4429b7b2da
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-12 18:12:54 +0200

    test: move examples to test file

--- argent-rose/ArgentRose.Tests/ArgentRoseTest.cs
+++ argent-rose/ArgentRose.Tests/ArgentRoseTest.cs
@@ -1,6 +1,13 @@
-
 using NUnit.Framework;
 
+// [(s:10, q:10)] => [(s:9, q:8)]
+// [(s:1, q:2)] => [(s:0, q:0)]
+// [(s:0, q:10)] => [(s:-1, q:6)]
+// [(s:0, q:4)] => [(s:-1, q:0)]
+// [(s:3, q:0)] => [(s:2, q:0)]
+// [(s:1, q:1)] => [(s:0, q:0)]
+// [(s:0, q:3)] => [(s:-1, q:0)]
+    
 namespace ArgentRose.Tests
 {
     public class ArgentRoseTest
