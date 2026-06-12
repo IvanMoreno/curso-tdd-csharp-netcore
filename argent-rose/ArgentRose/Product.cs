c0306b3287322489af17895ca19de3282b6ad691
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-12 19:52:24 +0200

    chore: describe products

--- argent-rose/ArgentRose/Product.cs
+++ argent-rose/ArgentRose/Product.cs
@@ -3,16 +3,22 @@ using System;
 namespace ArgentRose;
 
 public class Product {
+    readonly string description;
     readonly int sellIn;
     readonly Quality quality;
 
-    Product(int sellIn, Quality quality) {
+    Product(int sellIn, Quality quality, string description) {
         this.sellIn = sellIn;
         this.quality = quality;
+        this.description = description;
     }
 
-    public Product Update() => new(sellIn - 1, quality.DecreaseBy(Devaluation()));
+    public Product Update() => new(sellIn - 1, quality.DecreaseBy(Devaluation()), description);
     int Devaluation() => sellIn < 1 ? 4 : 2;
+
+    bool Equals(Product other) {
+        return description == other.description && sellIn == other.sellIn && quality.Equals(other.quality);
+    }
 
     public override bool Equals(object obj) {
         if (obj is null) return false;
@@ -21,19 +27,15 @@ public class Product {
         return Equals((Product)obj);
     }
 
-    bool Equals(Product other) {
-        return sellIn == other.sellIn && quality.Equals(other.quality);
-    }
-
     public override int GetHashCode() {
-        return HashCode.Combine(sellIn, quality);
+        return HashCode.Combine(description, sellIn, quality);
     }
 
     public override string ToString() {
-        return $"{nameof(sellIn)}: {sellIn}, {nameof(quality)}: {quality}";
+        return $"{nameof(description)}: {description}, {nameof(sellIn)}: {sellIn}, {nameof(quality)}: {quality}";
     }
 
     public static Product Regular(int sellIn, Quality quality) {
-        return new Product(sellIn, quality);
+        return new Product(sellIn, quality, "Regular");
     }
 }
