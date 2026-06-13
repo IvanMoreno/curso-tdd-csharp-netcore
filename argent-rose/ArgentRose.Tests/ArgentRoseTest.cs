30b77f09eadae3a552720c443a0fe23a307a0586
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-13 10:47:49 +0200

    test: parameterize test to cover boundary case

--- argent-rose/ArgentRose.Tests/ArgentRoseTest.cs
+++ argent-rose/ArgentRose.Tests/ArgentRoseTest.cs
@@ -4,7 +4,7 @@ using static ArgentRose.ProductFactory;
 // [x] Product has a description
 // [x] - [(s:7, q: 0)] => [(s:6, q: 1)]
 // [x] - [(s:6, q: 0)] => [(s:5, q: 3)]
-// [] - [(s:1, q: 0)] => [(s:0, q: 3)]
+// [x] - [(s:1, q: 0)] => [(s:0, q: 3)]
 // [] - [(s:0, q: 0)] => [(s:-1, q: 0)]
 // [x] - [(s:0, q: 40)] => [(s:-1, q: 0)]
 // [x] - [(s:7, q: 50)] => [(s:6, q: 50)]
@@ -78,23 +78,24 @@ namespace ArgentRose.Tests {
             Assert.That(sut, Is.EqualTo(new ArgentRoseStore([TheatrePass(sellIn: 6, quality: 1)])));
         }
 
-        [Test]
-        public void TheatrePassQuality_Increases_ByThree_WhenCloseToExpiration() {
-            var sut = new ArgentRoseStore([TheatrePass(sellIn: 6, quality: 0)]);
+        [TestCase(6, 0)]
+        [TestCase(1, 3)]
+        public void TheatrePassQuality_Increases_ByThree_WhenCloseToExpiration(int sellIn, int quality) {
+            var sut = new ArgentRoseStore([TheatrePass(sellIn, quality: quality)]);
 
             sut.Update();
 
-            Assert.That(sut, Is.EqualTo(new ArgentRoseStore([TheatrePass(sellIn: 5, quality: 3)])));
+            Assert.That(sut, Is.EqualTo(new ArgentRoseStore([TheatrePass(sellIn - 1, quality: quality + 3)])));
         }
 
         [TestCase(7, 50)]
         [TestCase(6, 48)]
         public void Quality_CannotBeGreater_ThanFifty(int sellIn, int quality) {
-            var sut = new ArgentRoseStore([TheatrePass(sellIn: sellIn, quality: quality)]);
+            var sut = new ArgentRoseStore([TheatrePass(sellIn, quality)]);
 
             sut.Update();
 
-            Assert.That(sut, Is.EqualTo(new ArgentRoseStore([TheatrePass(sellIn: sellIn - 1, quality: 50)])));
+            Assert.That(sut, Is.EqualTo(new ArgentRoseStore([TheatrePass(sellIn - 1, quality: 50)])));
         }
 
         [Test]
