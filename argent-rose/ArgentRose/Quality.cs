9ed2d3b0f8c066b1e2a7f7988afed920a14091d5
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-06-14 13:18:59 +0200

    refactor: remove quality thresholds duplication

--- argent-rose/ArgentRose/Quality.cs
+++ argent-rose/ArgentRose/Quality.cs
@@ -6,20 +6,22 @@ public readonly struct Quality {
     readonly int level;
 
     Quality(int level) {
-        if (level < 0 || level > 50)
+        if (level < Min || level > Max)
             throw new ArgumentException("Quality must be between 0 and 50");
         
         this.level = level;
     }
 
-    public Quality DecreaseBy(int amount) => Math.Max(level - amount, 0);
-    public Quality IncreaseBy(int addend) => Math.Min(level + addend, 50);
+    public Quality DecreaseBy(int amount) => Math.Max(level - amount, Min);
+    public Quality IncreaseBy(int addend) => Math.Min(level + addend, Max);
 
     public override string ToString() {
         return $"{nameof(level)}: {level}";
     }
     
     public static implicit operator Quality(int level) => new(level);
+    public static implicit operator int(Quality quality) => quality.level;
 
     public static Quality Min => 0;
+    public static Quality Max => 50;
 }
